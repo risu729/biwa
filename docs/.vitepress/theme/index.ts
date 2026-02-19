fab61bb211cda3b898ed66a076b15196336deb32
Risu <79110363+risu729@users.noreply.github.com>
2026-02-19 17:19:45 +1100

    feat: restore custom theme

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,8 @@
+import DefaultTheme from "vitepress/theme";
+
+// @ts-expect-error
+// eslint-disable-next-line eslint/sort-imports import/no-unassigned-import
+import "./custom.css";
+
+// eslint-disable-next-line import/no-default-export
+export default DefaultTheme;
