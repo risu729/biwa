4a3db17fd79916f8f3ea9bcaa4927902c48ec05e
Risu <79110363+risu729@users.noreply.github.com>
2026-02-19 15:50:02 +1100

    docs: remove custom theme

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,23 +7,21 @@ export default withMermaid(
 		description: "CLI to execute commands on UNSW CSE servers from local",
 		head: [["link", { href: "/icon.svg", rel: "icon" }]],
 		srcDir: "src",
-
 		themeConfig: {
 			// https://vitepress.dev/reference/default-theme-config
+			externalLinkIcon: true,
 			footer: {
 				copyright: 'Maintained by <a href="https://github.com/risu729">@risu729</a>',
 				message:
 					'Released under the <a href="https://github.com/risu729/biwa/blob/main/LICENSE">MIT License</a>.',
 			},
 			logo: "/icon.svg",
-
 			nav: [
 				{ link: "/", text: "Home" },
 				{ link: "/about", text: "About" },
 				{ link: "/getting-started", text: "Getting Started" },
 				{ link: "/configuration", text: "Configuration" },
 			],
-
 			sidebar: [
 				{
 					items: [
@@ -44,7 +42,6 @@ export default withMermaid(
 					text: "Community",
 				},
 			],
-
 			socialLinks: [{ icon: "github", link: "https://github.com/risu729/biwa" }],
 		},
 		title: "biwa",
